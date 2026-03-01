1105726a43359bd96bc514f74482283a0fcac82f
muqtasidbhatti <muqtasidbhatti246@gmail.com>
2026-03-02 00:45:33 +0500

    Restore routing and context setup after merge cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Notes from './components/Notes'
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
-const App = () => {
-  return (
-    <div>
-      <Notes/>
-    </div>
-  )
+
+function App() {
+  return (  
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/about" element={< About />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
