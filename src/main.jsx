d636352ca82299bd9dd2de71a337338c087a3654
muqtasidbhatti <muqtasidbhatti246@gmail.com>
2026-03-02 00:32:11 +0500

    Fix routing and context setup in main and App components

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-import { NotesProvider } from './context/notesContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <NotesProvider>
-        <App />
-      </NotesProvider>
-    </BrowserRouter>
-=======
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
->>>>>>> 5de4dc74d82a449e6fe2cbf52eeaf7e9d467afde
   </StrictMode>,
 )
